14d669aa1a6ad1466c8c953207b4ff50f2490593
sfh_hirabaru <hirabaru@softhousewave.jp>
2024-02-14 13:33:33 +0900

    リンクレア作成RPA

---
+++ アサヒ緑健RPA/02.配配メール _9通Ver/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace 配配メール自動プロジェクト_9通Ver
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
